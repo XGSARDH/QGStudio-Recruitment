6d600bfafb92c8ea45d93d9baf00a89253950c77
XGSARDH <xgsardh@163.com>
2024-03-17 22:32:39 +0800

    Can be directly compiled and run

--- Large group/Week_1/LinkList/duLinkList/Sources/main.c
+++ Large group/Week_1/LinkList/duLinkList/Sources/main.c
@@ -11,6 +11,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"..\Headers\DuLinkedList.h"
+#include"DuLinkedList.c" 
 
 /**************************************************************
 *	Prototype Declare Section
--- Large group/Week_1/LinkList/linkList/Sources/main.c
+++ Large group/Week_1/LinkList/linkList/Sources/main.c
@@ -11,6 +11,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"..\Headers\linkedList.h"
+#include"linkedList.c"
 
 /**************************************************************
 *	Prototype Declare Section
